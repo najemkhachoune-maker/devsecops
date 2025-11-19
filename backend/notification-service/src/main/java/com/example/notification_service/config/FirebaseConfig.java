4407e73c036da533295a08f65f1a95711dd85e58
Nadia Mallah <n.mallah2004@gmail.com>
2025-11-19 10:04:44 +0100

    Initial commit

---
+++ backend/notification-service/src/main/java/com/example/notification_service/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package com.example.notification_service.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initializeFirebase() throws IOException {
+        // Charge ton fichier JSON depuis /resources
+        try (InputStream serviceAccount =
+                     getClass().getResourceAsStream("/firebase-service-account.json")) {
+
+            if (serviceAccount == null) {
+                throw new IOException("Fichier firebase-service-account.json introuvable dans /resources !");
+            }
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("✅ Firebase initialisé avec succès !");
+            }
+        }
+    }
+}
