bf6c4c93023a7c9f983f8e37909c1f0e7429f134
Oumaima <bellarzaloumaima@gmail.com>
2025-11-24 15:02:08 +0100

    Ajout du microservice ProfilService

---
+++ backend/profilservice/src/test/java/com/skillverse/profilservice/ProfilServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillverse.profilservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
